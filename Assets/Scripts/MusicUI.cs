b4404a4be9b2d9bb27ee8d0495248bf0a834bef8
Benjamin Phillips <bdphillips013@gmail.com>
2021-09-25 17:40:12 -0400

    Adds music UI abstract stuff

---
+++ Assets/Scripts/MusicUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MusicUI: MusicUIBase
+{
+    public override void SetCurrentBeat(int beat)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SetBeatStatus(int beat, bool succeeded)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ResetAllStatuses()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/MusicUIBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class MusicUIBase: MonoBehaviour
+{
+    /// <summary>
+    /// | *  *  *  *  |  *  *  *  * |
+    ///
+    /// SetCurrentBeat(1)
+    ///
+    /// | * (*) *  *  |  *  *  *  * |
+    /// 
+    /// </summary>
+    /// <param name="beat"></param>
+    public abstract void SetCurrentBeat(int beat);
+
+    public abstract void SetBeatStatus(int beat, bool succeeded);
+
+    public abstract void ResetAllStatuses();
+}
