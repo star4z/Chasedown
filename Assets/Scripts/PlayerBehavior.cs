2c918ef471229b0850b33daf39b854078dcff9a2
Benjamin Phillips <bdphillips013@gmail.com>
2021-09-25 11:21:50 -0400

    Updates path following to use bezier curve lib

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PathCreation;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float velocity = 25;
+    private float _distanceTraveled;
+
+    private void Update()
+    {
+        _distanceTraveled += velocity * Time.deltaTime;
+        var pathPosition = pathCreator.path.GetPointAtDistance(_distanceTraveled, EndOfPathInstruction.Stop);
+        transform.position = new Vector3(pathPosition.x, transform.position.y, pathPosition.z);
+        // transform.position = pathPosition;
+        var pathRotation = pathCreator.path.GetRotationAtDistance(_distanceTraveled, EndOfPathInstruction.Stop);
+        transform.rotation = pathRotation;
+    }
+}
