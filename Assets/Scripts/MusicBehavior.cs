d9d4c35a704af756ca0aaf4931abb46a8fd7e129
Benjamin Phillips <bdphillips013@gmail.com>
2021-09-25 20:41:00 -0400

    Adds audio playback

---
+++ Assets/Scripts/MusicBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class MusicBehavior: MonoBehaviour
+{
+    public AudioSource audioSource;
+    private void Start()
+    {
+        PlayerBehavior.StartLevel += PlayerBehaviorOnStartLevel;
+    }
+
+    private void PlayerBehaviorOnStartLevel(object sender, EventArgs e)
+    {
+        audioSource.Play();
+    }
+}
