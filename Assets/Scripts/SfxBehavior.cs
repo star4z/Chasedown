02f0912e5210388104268432dc335bfc6663d9d9
Benjamin Phillips <bdphillips013@gmail.com>
2021-09-26 12:36:53 -0400

    Adds sfx to Canvas.prefab

---
+++ Assets/Scripts/SfxBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class SfxBehavior : MonoBehaviour
+{
+    public AudioSource laneChange;
+    public AudioSource crashing;
+    public AudioSource crashed;
+
+    private void Start()
+    {
+        Obstacle.OnEnter += ObstacleOnOnEnter;
+        PlayerBehavior.LaneChange += PlayerBehaviorOnLaneChange;
+    }
+
+    private void PlayerBehaviorOnLaneChange(object sender, EventArgs e)
+    {
+        laneChange.Play();
+    }
+
+    private void ObstacleOnOnEnter(object sender, Collider e)
+    {
+        if (sender is Obstacle { stopsThePlayer: true } o)
+        {
+            crashed.Play();
+        }
+        else
+        {
+            crashing.Play();
+        }
+    }
+}
