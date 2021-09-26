bbd445c03cdc172b5721a7a79aa4a30eda6ff7e5
Benjamin Phillips <bdphillips013@gmail.com>
2021-09-26 11:50:03 -0400

    Adds some much needed spice to the main menu

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour
+{
+    public int velocity = 5;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * velocity);
+    }
+}
